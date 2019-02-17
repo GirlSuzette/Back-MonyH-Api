144892808329542d4b0c0589613c45b7bfcf2eac
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2019-02-17 17:34:11 -0600

    erro model incomes

--- src/models/Income.js
+++ src/models/Income.js
@@ -1,6 +1,5 @@
 const ODM = require('mongoose')
 
-<<<<<<< HEAD
 const Schema = new ODM.Schema(
   {
     _id: ODM.Schema.Types.ObjectId,
@@ -20,31 +19,6 @@ const Schema = new ODM.Schema(
       type: String,
       required: true
     },
-    status: {
-      type: String,
-      required: true
-    },
-=======
-const Schema = new ODM.Schema({
-  _id: ODM.Schema.Types.ObjectId,
-  concept: {
-    type: String,
-    required: true
-  },
-  quantity: {
-    type: Number,
-    required: true
-  },
-  date: {
-    type: String,
-    required: true
-  },
-  type: {
-    type: String,
-    required: true
-  },
->>>>>>> 9d3e320184306b5888cdc7b29c02e4f254090a00
-
     user: {
       type: ODM.Schema.Types.ObjectId,
       ref: 'User'
